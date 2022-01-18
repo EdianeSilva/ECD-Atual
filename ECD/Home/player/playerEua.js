1172f83a601344deeda8e6b9c4649d005a8286c8
Ediane Silva <ediane.web@gmail.com>
2022-01-18 13:16:36 -0300

    ...

--- ECD/Home/player/playerEua.js
+++ ECD/Home/player/playerEua.js
@@ -1,5 +1,4 @@
 //Player Radio EUA
-
 let musica = document.querySelector('.eua');
 let indexMusica = 0;
 
@@ -22,3 +21,24 @@ function pausarMusica(){
     document.querySelector('.botao-play').style.display = 'block';
 }
 
+// Player Musicas brasil
+let musicabr = document.querySelector('.br');
+let indexMusicabr = 0;
+
+
+// Eventos
+document.querySelector('.botao-play2').addEventListener('click', tocarMusicabr);
+
+document.querySelector('.botao-pause2').addEventListener('click', pausarMusicabr);
+
+function tocarMusicabr(){
+    musicabr.play();
+    document.querySelector('.botao-pause2').style.display = 'block';
+    document.querySelector('.botao-play2').style.display = 'none';
+}
+
+function pausarMusicabr(){
+    musicabr.pause();
+    document.querySelector('.botao-pause2').style.display = 'none';
+    document.querySelector('.botao-play2').style.display = 'block';
+}
