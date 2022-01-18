61b122f5b01541be24d3965fe91a8958a9931b2c
Ediane Silva <ediane.web@gmail.com>
2022-01-18 12:37:27 -0300

    ...

--- ECD/Home/player/playerBr.js
+++ ECD/Home/player/playerBr.js
@@ -8,13 +8,13 @@ document.querySelector('.botao-play2').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause2').addEventListener('click', pausarMusica);
 
-function tocarAudio(){
+function tocarMusica(){
     Audio.play();
     document.querySelector('.botao-pause2').style.display = 'block';
     document.querySelector('.botao-play2').style.display = 'none';
 }
 
-function pausarAudio(){
+function pausarMusica(){
     Audio.pause();
     document.querySelector('.botao-pause2').style.display = 'none';
     document.querySelector('.botao-play2').style.display = 'block';
